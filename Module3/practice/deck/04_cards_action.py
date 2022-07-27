c02453f38781db67ac497f09db5147c76f0f5363
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-07-27 21:09:00 +0300

    Update 04_cards_action.py

--- Module3/practice/deck/04_cards_action.py
+++ Module3/practice/deck/04_cards_action.py
@@ -7,6 +7,12 @@ cards = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 # TODO-1: в список cards добавьте ВСЕ карты всех мастей
-
+for value in values:
+    for suit in suits:
+        cards.append(Card(value, suit))
 # TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
-#  кол-во берем от размера списка cards
+i = 1
+for card in hearts_cards:
+    cards.append(card.to_str())
+    i += 1
+print('cards', i, ','.join(hearts_cards_str))
